c97eb3c35250728a8bc3d30de7a903a6d760f456
Bernhard Doppler <github@doppera.at>
2026-01-27 22:00:32 +0100

    add logger from old codebase

---
+++ src/utils/Logger.ts
@@ -0,0 +1,64 @@
+export enum Level {
+    ERROR = 20,
+    WARN = 30,
+    STAT = 50,
+    INFO = 60,
+    DEBUG = 100,
+    FINE = 150,
+    FINER = 200,
+    ALL = 255,
+}
+
+export class Logger {
+
+    private module: string;
+    private method: string;
+    private level: Level;
+
+    constructor(module: string, method: string, level: Level) {
+        this.module = module;
+        this.method = method;
+        this.level = level;
+    }
+
+
+    public log(message: string, level: Level, result?: number) {
+        if (level > this.level) return;
+
+        let resultString = `[${Level[level]}] ${this.module}:${this.method}> ${message}`;
+        if (result !== undefined) resultString += ` (${result}) ${this.getResultString(result)}`;
+
+        console.log(resultString);
+    }
+
+    public error(message: string, result?: number) {
+        this.log(message, Level.ERROR, result);
+    }
+    public warn(message: string, result?: number) {
+        this.log(message, Level.WARN, result);
+    }
+    public stat(message: string, result?: number) {
+        this.log(message, Level.STAT, result);
+    }
+    public info(message: string, result?: number) {
+        this.log(message, Level.INFO, result);
+    }
+    public debug(message: string, result?: number) {
+        this.log(message, Level.DEBUG, result);
+    }
+    public fine(message: string, result?: number) {
+        this.log(message, Level.FINE, result);
+    }
+    public finer(message: string, result?: number) {
+        this.log(message, Level.FINER, result);
+    }
+
+
+    private getResultString(result: number) {
+        switch (result) {
+            case OK: return "OK";
+            case ERR_NOT_FOUND: return "NotFound";
+            default: return "";
+        }
+    }
+}
