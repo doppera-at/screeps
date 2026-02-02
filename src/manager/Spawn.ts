7cae5339349caae4206349567211b02b8aaea026
doppera-at <github@doppera.at>
2026-02-02 15:06:40 +0100

    add check for available energy, refactor for better readability

--- src/manager/Spawn.ts
+++ src/manager/Spawn.ts
@@ -2,6 +2,7 @@ import { Logger } from "../utils/Logger";
 import { getName } from "../utils/Names";
 import { ActionType, TaskType } from "../utils/TypeDefinitions";
 
+const BODY_DEFAULT: BodyPartConstant[] = [WORK, CARRY, MOVE];
 
 export class SpawnManager {
 
@@ -31,22 +32,42 @@ export class SpawnManager {
 
 
   public run(spawnName: string) {
-    if (Object.keys(Game.creeps).length < 2) {
-      const creepName = getName();
-      this.logger.debug(`Spawning Creep: ${creepName}`);
-      const spawn = this.spawns[spawnName] ?? null;
-      if (!spawn) {
-        this.logger.error(`Invalid spawnName: ${spawnName}`);
-      }
-      const result = spawn.spawnCreep([WORK, CARRY, MOVE], creepName, {
-        memory: {
-          task: TaskType.HARDCODED,
-          action: ActionType.HARVEST,
-          spawn: spawn.id,
-          room: spawn.room.name,
-        }
-      });
-      this.logger.info(`Attempt of spawning a creep`, result);
+    if (Object.keys(Game.creeps).length > 2) {
+      return;
     }
+
+    const spawn = this.spawns[spawnName] ?? null;
+    if (!spawn) {
+      this.logger.error(`Invalid spawnName: ${spawnName}`);
+      return;
+    }
+
+    const spawnEnergy = spawn.store.getUsedCapacity(RESOURCE_ENERGY)
+    const bodyCost = this.getCostForBody(BODY_DEFAULT)
+    if (spawnEnergy < bodyCost) {
+      this.logger.info(`Skipping spawn because lack of energy: ${spawnEnergy} < ${bodyCost} (Body: ${BODY_DEFAULT})`)
+      return;
+    }
+
+    const creepName = getName();
+    this.logger.debug(`Spawning Creep: ${creepName}`);
+    const result = spawn.spawnCreep(BODY_DEFAULT, creepName, {
+      memory: {
+        task: TaskType.HARDCODED,
+        action: ActionType.HARVEST,
+        spawn: spawn.id,
+        room: spawn.room.name,
+      }
+    });
+    this.logger.info(`Attempt of spawning a creep`, result);
+  }
+
+
+  public getCostForBody(body: BodyPartConstant[]): number {
+    let cost = 0;
+    for (const part of body) {
+      cost += BODYPART_COST[part] | 0;
+    }
+    return cost;
   }
 }
